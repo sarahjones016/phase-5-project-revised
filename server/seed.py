7ac2f9615b9e041d4df6763cd343aa6cb163b3b7
Sarah Jones <sarahrjones8@gmail.com>
2023-06-13 15:40:22 -0400

    created models and seeded a sample instance for each table

--- server/seed.py
+++ server/seed.py
@@ -3,15 +3,29 @@
 # Standard library imports
 from random import randint, choice as rc
 
-# Remote library imports
-from faker import Faker
-
 # Local imports
 from app import app
-from models import db
+from models import db, User, Daily_Consumption, Drink, Drink_Type
+from datetime import date
 
-if __name__ == '__main__':
-    fake = Faker()
-    with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+with app.app_context():
+    print("Starting seed...")
+    # Seed code goes here!
+
+    User.query.delete()
+    Daily_Consumption.query.delete()
+    Drink.query.delete()
+    Drink_Type.query.delete()
+
+    user_test = User(email='sarah@gmail.com', _password_hash="test", first_name="Sarah", last_name="Jones")
+    daily_consumption_test = Daily_Consumption(date=date(1999, 0o2, 0o5), user_id=1, consumption_goal=12, ounces_consumed=3)
+    drink_test = Drink(daily_consumption_id=2, drink_type_id=3, ounces=1)
+    drink_type_test = Drink_Type(name="water")
+
+    db.session.add(user_test)
+    db.session.add(daily_consumption_test)
+    db.session.add(drink_test)
+    db.session.add(drink_type_test)
+
+    db.session.commit()
+
