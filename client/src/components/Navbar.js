7e4fa74c3d481f0183b7d2ae1a1551d5632f26a5
Sarah Jones <sarahrjones8@gmail.com>
2023-06-14 10:00:26 -0400

    navbar CSS

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./Navbar.css";
 import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IconContext } from "react-icons";
 
 function Navbar({user, setUser}) {
+
+  const [navButton, setNavButton] = useState(false)
+  const [showNavBar, setShowNavBar] = useState(false)
+
+    function handleNavClick() {
+        console.log("nav bar has been clicked")
+        setShowNavBar(!showNavBar)
+        setNavButton(!navButton)
+    } 
 
   function handleLogoutClick() {
     fetch("/logout", { method: "DELETE" }).then((r) => {
@@ -13,19 +24,33 @@ function Navbar({user, setUser}) {
   }
 
   return (
-    <div>
-      <ul>
+    <div className='hamburgerMenuDiv'>
+        
+    {showNavBar ? <div className='navButtonTwo'>
+        <IconContext.Provider className='hamburgerMenuTwo' value={{ size: "25px", color: "black"}}>
+            <RxHamburgerMenu onClick={handleNavClick}/>
+        </IconContext.Provider>            
+    </div> : <div className='navButton'>
+        <IconContext.Provider className='hamburgerMenu' value={{ size: "25px"}}>
+            <RxHamburgerMenu onClick={handleNavClick}/>
+        </IconContext.Provider>
+    </div>}
+    
+    {showNavBar ? <div className='navBar'>
+        <ul onClick={handleNavClick} className='options'>
         <li ><Link to='/portal'>Portal</Link></li>
         <li><Link to='/water-knowledge'>Water Knowledge</Link></li>
         <li>
           {user ? (
             <div>
-                <button onClick={handleLogoutClick}>Logout</button>
+                <button className='logout-button' onClick={handleLogoutClick}>Logout</button>
             </div>
           ) : null}
         </li>
-      </ul>
-    </div>
+        </ul>
+    </div> : null}
+    
+</div>
   )
 }
 
