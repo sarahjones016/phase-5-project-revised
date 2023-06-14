5b812a5d7d897b235e632a06569a7e4f062ecfa5
Sarah Jones <sarahrjones8@gmail.com>
2023-06-14 10:18:17 -0400

    portal page split

--- client/src/components/Portal.js
+++ client/src/components/Portal.js
@@ -1,10 +1,24 @@
 import React from 'react'
 import "./Portal.css";
+import NewDayForm from './NewDayForm'
+import MostRecentDay from './MostRecentDay'
+import DaysList from './DaysList'
 
 function Portal() {
   return (
-    <div>
-      <h1 className='h1'>Portal</h1>
+    <div className='portal'>
+      {/* <h1 className='h1'>Portal</h1> */}
+      <div className='left-panel'>
+        <NewDayForm />
+      </div>
+      <div className='right-panel'>
+        <div className='right-top'>
+           <MostRecentDay />
+        </div>
+        <div className='right-bottom'>
+          <DaysList />
+        </div>
+      </div>
     </div>
   )
 }
